fce62efa49b9653717d75b444db1267117ac47f3
unknown <arundk2000@gmail.com>
2025-07-16 15:26:38 +0530

    Fix: Added CORS configuration for frontend access

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import http from 'http';
 import 'dotenv/config';
 import App from './App.js';
 import connectDB from './Database/db.connect.js';
+import cors from "cors";
+app.use(cors({
+  origin: "https://bankfrontend-2.onrender.com", // your frontend URL
+  credentials: true // optional: only if you're using cookies
+}));
+
 const server=http.createServer(App);
 
 const PORT=process.env.PORT
